4444886da3c7b859f2f265a67f3802af8c38e1bb
umbiumbiian <futihmilla003@upi.edu>
2025-06-20 11:06:05 +0700

    model baru

---
+++ lib/models/ekspedisi_model.dart
@@ -0,0 +1,13 @@
+class EkspedisiModel {
+  
+  final int id;
+  final String name;
+  final int price;
+
+  EkspedisiModel({
+    required this.id, 
+    required this.name, 
+    required this.price
+    });
+  
+}
